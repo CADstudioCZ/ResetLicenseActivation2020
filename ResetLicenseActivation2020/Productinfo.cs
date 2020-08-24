078f7dffe49f71470280890f08227966140c7425
Navara Michael <Michael.Navara@cadstudio.cz>
2020-08-24 09:06:09 +0200

    Initial commit

---
+++ ResetLicenseActivation2020/Productinfo.cs
@@ -0,0 +1,112 @@
+﻿using System.Runtime.Serialization;
+
+
+//public class ProductInfos
+//{
+//    public ProductInfo[] Property1 { get; set; }
+//}
+
+
+public class ProductInfo
+{
+    public bool authorize_succ { get; set; }
+
+
+    public string background { get; set; }
+
+
+    public bool cls_check_succ { get; set; }
+
+
+    public string def_prod_code { get; set; }
+
+
+    public string def_prod_key { get; set; }
+
+
+    public string def_prod_ver { get; set; }
+
+
+    public string feature_id { get; set; }
+
+
+    //public Highlight highlight { get; set; }
+
+
+    public string icon { get; set; }
+
+
+    public string last_known_expire_date { get; set; }
+
+
+    public int lic_method { get; set; }
+
+
+    public int lic_server_type { get; set; }
+
+
+    public string[] lic_servers { get; set; }
+
+
+    public string sel_prod_code { get; set; }
+
+
+    public string sel_prod_key { get; set; }
+
+
+    public string sel_prod_ver { get; set; }
+
+
+    public string serial_number_nw { get; set; }
+
+
+    public string serial_number_sa { get; set; }
+
+
+    public int[] supported_lic_methods { get; set; }
+
+
+    public bool trial_enabled { get; set; }
+
+
+    public bool user_lic_enabled { get; set; }
+}
+/*
+public class Highlight
+{
+    public Alpha alpha { get; set; }
+
+
+    public Blue blue { get; set; }
+
+
+    public Green green { get; set; }
+
+
+    public Red red { get; set; }
+}
+
+
+public class Red
+{
+    public int value { get; set; }
+}
+
+
+public class Green
+{
+    public int value { get; set; }
+}
+
+
+public class Blue
+{
+    public int value { get; set; }
+}
+
+
+public class Alpha
+{
+    public int value { get; set; }
+}
+*/
---
+++ ResetLicenseActivation2020/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace ResetLicense
+{
+    class Program
+    {
+        private static string LicHelperExe => Environment.ExpandEnvironmentVariables(
+            @"%CommonProgramFiles(x86)%\Autodesk Shared\AdskLicensing\Current\helper\AdskLicensingInstHelper.exe");
+
+        private static string LoginStateXml =>
+            Environment.ExpandEnvironmentVariables(@"%LOCALAPPDATA%\Autodesk\Web Services\LoginState.xml");
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"
+Automatized action described here:
+https://knowledge.autodesk.com/support/autocad/troubleshooting/caas/sfdcarticles/sfdcarticles/Forcing-re-activation-of-product.html#subscription
+");
+            //Read license content
+            string outputContent = ReadLicenseContent();
+            
+            //Parse productInfos
+            var productInfos = Newtonsoft.Json.JsonConvert.DeserializeObject<ProductInfo[]>(outputContent);
+            PrintFoundLicenses(productInfos);
+
+            //Unregister
+            UnregisterLicenses(productInfos);
+
+            //Delete LoginState.xml
+            DeleteLoginStateXml();
+
+            //Kill process AdSSO.exe
+            KillAdSSOExe();
+
+            Console.WriteLine("GAME OVER");
+        }
+
+        private static void UnregisterLicenses(ProductInfo[] productInfos)
+        {
+            foreach (var productInfo in productInfos)
+            {
+                UnregisterLicense(productInfo);
+            }
+        }
+
+        private static void PrintFoundLicenses(ProductInfo[] productInfos)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Products");
+            foreach (var productInfo in productInfos)
+            {
+                Console.WriteLine($"{productInfo.def_prod_ver}\t{productInfo.def_prod_code}");
+            }
+        }
+
+        private static void DeleteLoginStateXml()
+        {
+            if (File.Exists(LoginStateXml))
+            {
+                File.Delete(LoginStateXml);
+                Console.WriteLine("Successfuly deleted");
+            }
+            else
+                Console.WriteLine($"File not found\r\n{LoginStateXml}");
+        }
+
+        private static string ReadLicenseContent()
+        {
+            ProcessStartInfo psi = new ProcessStartInfo(LicHelperExe, "list")
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+
+            var standardOutput = Process.Start(psi)?.StandardOutput;
+            string outputContent = standardOutput?.ReadToEnd();
+            Console.WriteLine(outputContent);
+            return outputContent;
+        }
+
+        private static void UnregisterLicense(ProductInfo productInfo)
+        {
+            string prod_key = productInfo.def_prod_key;
+            string prod_ver = productInfo.def_prod_ver;
+            string arg = $"change --prod_key {prod_key} --prod_ver {prod_ver} --lic_method \"\"";
+
+            ProcessStartInfo unLicStartinfo = new ProcessStartInfo(LicHelperExe)
+            {
+                Arguments = arg,
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+
+            var unLicProcess = Process.Start(unLicStartinfo);
+            unLicProcess?.WaitForExit();
+            Console.WriteLine($"DONE AdskLicensingInstHelper.exe {arg}");
+        }
+
+        private const string KillInventorFile = "Kill_AdSSOexe.bat";
+        private const string KillInventorContent = @"TASKKILL /F /IM AdSSO.exe /T";
+
+        public static void KillAdSSOExe()
+        {
+            EnsureBatchFile(KillInventorFile, KillInventorContent);
+            ExecuteBatchFile(KillInventorFile);
+            Console.WriteLine("Process AdSSO.exe killed.");
+        }
+
+        private static void EnsureBatchFile(string batFileName, string batFileContent)
+        {
+            if (!File.Exists(batFileName))
+                File.WriteAllText(batFileName, batFileContent, Encoding.GetEncoding(852));
+        }
+
+        private static void ExecuteBatchFile(string batFileName)
+        {
+            var process = Process.Start(batFileName);
+            process?.WaitForExit(1000);
+        }
+    }
+}
